2dbb5c172ee7124fc09dfa828faa3d55f4baa46b
Edgar Mesquita <edgar.rj@gmail.com>
2019-10-10 20:41:42 +0100

    async write methods

--- src/Repository/Write/IAsyncWriteRepository.cs
+++ src/Repository/Write/IAsyncWriteRepository.cs
@@ -7,26 +7,50 @@ namespace eQuantic.Core.Data.Repository.Write
 {
     public interface IAsyncWriteRepository<TUnitOfWork, TEntity, TKey> : IAsyncWriteRepository<TEntity, TKey>, IAsyncRepository<TUnitOfWork>
         where TUnitOfWork : IUnitOfWork
-        where TEntity : class, IEntity, new()
-    {
-    }
+    where TEntity : class, IEntity, new () { }
 
     public interface IAsyncWriteRepository<TEntity, TKey> : IAsyncRepository
-            where TEntity : class, IEntity, new()
+    where TEntity : class, IEntity, new ()
     {
+        /// <summary>
+        /// Add item into repository
+        /// </summary>
+        /// <param name="item">Item to add to repository</param>
+        Task AddAsync (TEntity item);
+
         /// <summary>
         /// Delete filtered elements of type TEntity in repository
         /// </summary>
         /// <param name="filter"></param>
         /// <returns></returns>
-        Task<int> DeleteManyAsync(Expression<Func<TEntity, bool>> filter);
+        Task<int> DeleteManyAsync (Expression<Func<TEntity, bool>> filter);
 
         /// <summary>
         /// Delete specified elements of type TEntity in repository
         /// </summary>
         /// <param name="specification"></param>
         /// <returns></returns>
-        Task<int> DeleteManyAsync(ISpecification<TEntity> specification);
+        Task<int> DeleteManyAsync (ISpecification<TEntity> specification);
+
+        /// <summary>
+        /// Sets modified entity into the repository. When calling Commit() method in UnitOfWork
+        /// these changes will be saved into the storage
+        /// </summary>
+        /// <param name="persisted">The persisted item</param>
+        /// <param name="current">The current item</param>
+        Task MergeAsync (TEntity persisted, TEntity current);
+
+        /// <summary>
+        /// Set item as modified
+        /// </summary>
+        /// <param name="item">Item to modify</param>
+        Task ModifyAsync (TEntity item);
+
+        /// <summary>
+        /// Delete item
+        /// </summary>
+        /// <param name="item">Item to delete</param>
+        Task RemoveAsync (TEntity item);
 
         /// <summary>
         /// Update filtered elements of type TEntity in repository
@@ -34,7 +58,7 @@ namespace eQuantic.Core.Data.Repository.Write
         /// <param name="filter"></param>
         /// <param name="updateFactory"></param>
         /// <returns></returns>
-        Task<int> UpdateManyAsync(Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TEntity>> updateFactory);
+        Task<int> UpdateManyAsync (Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TEntity>> updateFactory);
 
         /// <summary>
         /// Update specified elements of type TEntity in repository
@@ -42,6 +66,6 @@ namespace eQuantic.Core.Data.Repository.Write
         /// <param name="specification"></param>
         /// <param name="updateFactory"></param>
         /// <returns></returns>
-        Task<int> UpdateManyAsync(ISpecification<TEntity> specification, Expression<Func<TEntity, TEntity>> updateFactory);
+        Task<int> UpdateManyAsync (ISpecification<TEntity> specification, Expression<Func<TEntity, TEntity>> updateFactory);
     }
 }
